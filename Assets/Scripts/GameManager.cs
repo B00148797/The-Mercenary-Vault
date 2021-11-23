73f4c4c4a09fed5eb3303f5ae63268293a19404c
ldestenay <B00148802@mytudublin.ie>
2021-11-23 15:45:08 +0000

    First Version GUI finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,24 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject titleScreen;
+    public GameObject room;
+    public GameObject creditsScreen;
 
     public void StartGame()
     {
         titleScreen.SetActive(false);
+        room.SetActive(true);
+    }
+
+    public void ShowCredits()
+    {
+        titleScreen.SetActive(false);
+        creditsScreen.SetActive(true);
+    }
+
+    public void ExitCredits()
+    {
+        creditsScreen.SetActive(false);
+        titleScreen.SetActive(true);
     }
 }
