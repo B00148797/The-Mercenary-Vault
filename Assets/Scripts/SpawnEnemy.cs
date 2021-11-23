13e151355b4fe3108e9dbe1b4f6942052ec46680
Thomas Perisse <B00148797@mytudublin.ie>
2021-11-23 20:55:29 +0000

    Automatic generation of enemy and loot patterns.

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    public GameObject[] patternEnemyList;
+    public GameObject[] lootList;
+
+    private int rand;
+
+    void Start()
+    {
+        rand = Random.Range(0, 4);
+
+        switch (rand)
+        {
+            case 0:
+                Instantiate(lootList[rand], transform.position, lootList[rand].transform.rotation);
+                break;
+            default:
+                rand = Random.Range(0, patternEnemyList.Length);
+                Instantiate(patternEnemyList[rand], transform.position, patternEnemyList[rand].transform.rotation);
+                break;
+        }
+    }
+}
